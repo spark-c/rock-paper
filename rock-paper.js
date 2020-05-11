6a4ae1d61dec473b8aa88605bf6bbfb383522923
sparksie <cklsparks@gmail.com>
2020-05-11 13:39:24 -0500

    completed rock-paper console version, Bo5 w tiebreakers.

--- rock-paper.js
+++ rock-paper.js
@@ -3,7 +3,7 @@
 
 function getComputerPick() {
     let choice = randomNumber(1, 4);
-    console.log(`choice is ${choice}`);
+    //console.log(`choice is ${choice}`);
     if(choice === 1) {
         let computerPick = 'rock'
         return computerPick;
@@ -18,28 +18,25 @@ function getComputerPick() {
 
 function randomNumber(min, max) {  
     let result = Math.random() * (max - min) + min;
-    console.log(result);
+    //console.log(result);
     return Math.floor(result);
 } 
 
 function getPlayerPick() {
-    do{
-        //let input = input
-    }
-    while(validateInput(input) === false) {
-        let input = input
-    }
-    return input
+    let userInput = prompt('Your choice:');
+    return userInput
 }
 
-function playRound(input=getPlayerPick()) {
-    if(validateInput(input) === false) {
-        return 'Invalid input'
-    } else {
-        let computerPick = getComputerPick();
-        outcome = getOutcome(input, computerPick);
-        return outcome;
+function playRound() {
+    let valid = false;
+    var input = ''
+    while(valid === false) {
+        input = getPlayerPick();
+        valid = validateInput(input);
     }
+    let computerPick = getComputerPick();
+    outcome = getOutcome(input, computerPick);
+    return outcome;
 }
 
 function validateInput(input) {
@@ -54,33 +51,85 @@ function validateInput(input) {
 function getOutcome(userPick, computerPick) {
     if(userPick === 'rock') {
         if(computerPick === 'paper') {
-            return 'You lose! Paper beats rock.';
+            let winner = 'Computer';
+            let msg = 'Round loss!! Paper beats rock.';
+            return [winner,msg];
         } else if(computerPick === 'scissors') {
-            return 'You win! Rock beats scissors.';
+            let winner = 'Player';
+            let msg = 'Round win!! Rock beats scissors.';
+            return [winner,msg];
         } else {
-            return 'Stalemate!';
+            let winner = null;
+            let msg = 'Stalemate!';
+            return [winner,msg];
         }
     } else if(userPick === 'paper') {
         if(computerPick === 'scissors') {
-            return 'You lose! Scissors beats paper.';
+            let winner = 'Computer';
+            let msg = 'Round loss!! Scissors beats paper.';
+            return [winner,msg];
         } else if(computerPick === 'rock') {
-            return 'You win! Paper beats rock.';
+            let winner = 'Player';
+            let msg = 'Round win!! Paper beats rock.';
+            return [winner,msg];
         } else {
-            return 'Stalemate!';
+            let winner = null;
+            let msg = 'Stalemate!';
+            return [winner,msg];
         }
     } else {
         if(computerPick === 'rock') {
-            return 'You lose! Rock beats scissors.';
+            let winner = 'Computer';
+            let msg = 'Round loss!! Rock beats scissors.';
+            return [winner,msg];
         } else if(computerPick === 'paper') {
-            return 'You win! Scissors beats paper.';
+            let winner = 'Player';
+            let msg = 'Round win!! Scissors beats paper.';
+            return [winner,msg];
         } else {
-            return 'Stalemate!';
+            let winner = null;
+            let msg = 'Stalemate!';
+            return [winner,msg];
         }
     }
 }
 
-function game() {
+function updateScore(outcomeReturn, scoresheet) {
+    let winner = outcomeReturn[0];
+    let msg = outcomeReturn[1];
+    if(winner === null) {
+        showScoreboard(msg, scoresheet);
+        return scoresheet;
+    }
+    scoresheet[winner]++;
+    showScoreboard(msg, scoresheet);
+    return scoresheet
+}
 
+function showScoreboard(msg, scoresheet) {
+    let round = scoresheet['Round'];
+    alert(`${msg}\nScore after Round ${round}:\nPlayer: ${scoresheet.Player}\nComputer: ${scoresheet.Computer}`);
+}
+
+function game() {
+    var currentScores = {'Player': 0, 'Computer': 0, 'Round': 0};
+    for(let i = 1; i < 6; i++) {
+        currentScores['Round'] = i
+        let result = playRound();
+        currentScores = updateScore(result, currentScores);
+    }
+    while(currentScores.Player === currentScores.Computer) {
+        alert('TIEBREAKER ROUND!');
+        let result = playRound();
+        currentScores = updateScore(result, currentScores);
+    }
+    if(currentScores.Player > currentScores.Computer) {
+        var gameWinner = 'Player';
+    } else {
+        var gameWinner = 'Computer';
+    }
+    alert (`${gameWinner} is the winner!!`);
 }
 
 console.log('Welcome!');
+console.log('Call "game()" to play rock-paper-scissors, Bo5 with tiebreakers.');
